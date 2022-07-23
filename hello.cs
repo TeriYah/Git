ad4c4ddd4444fed6d23f4fc01a4db81911cbd31c
teriyah <ebonimamasangel@gmail.com>
2022-07-23 15:21:50 -0700

    first commit

---
+++ hello.cs
@@ -0,0 +1,16 @@
+using System;
+namespace MyNamespace
+{
+    //c# compiling test run
+    class program
+    {
+        //main function
+        static void Main(string[] args)
+        {
+            //writing hello world to the screen
+            Console.WriteLine("Hello World");
+            Console.Read();
+        }
+
+    }
+}
